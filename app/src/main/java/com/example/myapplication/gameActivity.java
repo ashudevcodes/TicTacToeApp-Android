f1c37aa7775d0999e78668538b2287c1171d51ed
Ashish <ashishprasad949@gmail.com>
2023-07-03 19:31:29 +0530

    Fix big 1. Now 1 player will Won. 2. Resat game fix now it will clear all the move, not show new activity.

--- app/src/main/java/com/example/myapplication/gameActivity.java
+++ app/src/main/java/com/example/myapplication/gameActivity.java
@@ -4,11 +4,84 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import java.util.Scanner;
+
 
 public class gameActivity extends AppCompatActivity {
+
+    boolean gameActive = true;
+    int activePlayer = 0;
+    int[] gameState = {2, 2 , 2, 2, 2, 2, 2, 2, 2};
+    int[][] winPositions = {{0,1,2}, {3,4,5}, {6,7,8},
+            {0,3,6}, {1,4,7}, {2,5,8},
+            {0,4,8}, {2,4,6}};
+    public void buttonClick(View view){
+        ImageView img = (ImageView) view;
+        int tappedImage = Integer.parseInt(img.getTag().toString());
+        if(!gameActive){
+            gameReset(view);
+        }
+        if(gameState[tappedImage] == 2) {
+            gameState[tappedImage] = activePlayer;
+            if (activePlayer == 0) {
+                img.setImageResource(R.drawable.oig__3__removebg_preview__1_);
+                activePlayer = 1;
+                TextView status = findViewById(R.id.textView5);
+                status.setText("O's Turn");
+            } else {
+                img.setImageResource(R.drawable.oig_removebg_preview_removebg_preview);
+                activePlayer = 0;
+                TextView status = findViewById(R.id.textView5);
+                status.setText("X's Turn");
+            }
+        }
+        for(int[] winPosition: winPositions){
+            if(gameState[winPosition[0]] == gameState[winPosition[1]] &&
+                    gameState[winPosition[1]] == gameState[winPosition[2]] &&
+                    gameState[winPosition[0]]!=2){
+                String winnerStr;
+                gameActive = false;
+                if(gameState[winPosition[0]] == 0){
+                    winnerStr = "X won!";
+                }
+                else{
+                    winnerStr = "O Won!";
+                }
+                TextView status = findViewById(R.id.textView5);
+                status.setText(winnerStr);
+
+            }
+
+
+
+        }
+
+    }
+
+    public void gameReset(View view) {
+        gameActive = true;
+        activePlayer = 0;
+        for(int i=0; i<gameState.length; i++){
+            gameState[i] = 2;
+        }
+        ((ImageView)findViewById(R.id.imageView0)).setImageResource(0);
+        ((ImageView)findViewById(R.id.imageView1)).setImageResource(0);
+        ((ImageView)findViewById(R.id.imageView2)).setImageResource(0);
+        ((ImageView)findViewById(R.id.imageView3)).setImageResource(0);
+        ((ImageView)findViewById(R.id.imageView4)).setImageResource(0);
+        ((ImageView)findViewById(R.id.imageView5)).setImageResource(0);
+        ((ImageView)findViewById(R.id.imageView6)).setImageResource(0);
+        ((ImageView)findViewById(R.id.imageView7)).setImageResource(0);
+        ((ImageView)findViewById(R.id.imageView8)).setImageResource(0);
+
+        TextView status = findViewById(R.id.textView5);
+        status.setText("X's Turn");
+
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,44 +89,5 @@ public class gameActivity extends AppCompatActivity {
         setContentView(R.layout.activity_game);
     }
 
-    public void backToHome(View v){
-        Intent i=new Intent(this, MainActivity.class);
-        startActivity(i);
-    }
-
-    public void restart(View v){
-        Intent i=new Intent(this,gameActivity.class);
-        startActivity(i);
-    }
-    int[] gameState={2,2,2,2,2,2,2,2,2,2};
-    int player=0;
-    public void imageClicked(View v){
-        v.setEnabled(false);
-        ImageView img=(ImageView) v;
-        int tappedImage=Integer.parseInt(img.getTag().toString());
-        if(gameState[tappedImage]==2){
-            gameState[tappedImage]= player;
-            if(player ==0){
-                img.setImageResource(R.drawable.oig__1_);
-                Intent i=getIntent();
-                String playerMove1=i.getStringExtra("move");
-                ((TextView)findViewById(R.id.textView5)).setText(playerMove1);
-                player =1;
-
-
-
-
-
-            }
-            else{
-                img.setImageResource(R.drawable.oig_removebg_preview);
-                Intent i=getIntent();
-                String playerMove2=i.getStringExtra("move2");
-                ((TextView)findViewById(R.id.textView5)).setText(playerMove2);
-
-                player =0;
-            }
-        }
-        }
 
 }
